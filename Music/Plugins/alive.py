e51c3ce39322b47ca4511ac57c410c072a56628a
xylanzii <102651517+xylanzii@users.noreply.github.com>
2022-04-05 05:10:52 +0700

    Update alive.py

--- Music/Plugins/alive.py
+++ Music/Plugins/alive.py
@@ -42,7 +42,7 @@ async def _human_time_duration(seconds):
     return ', '.join(parts)
 
 
-KYY_IMG = "https://telegra.ph/file/2b610c918dec590d2777b.jpg"
+KYY_IMG = "https://telegra.ph/Vylan-Robot-04-04"
 
 
 @app.on_message(filters.command(["alive", "alive@Tg_Vc_00_Bot"]))
